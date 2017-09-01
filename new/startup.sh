5e6fafeae1f31a7f414e3693a1774703b3db3b13
RattyDAVE <dave@pucknell.co.uk>
2017-09-01 23:37:54 +0100

    Create startup.sh

---
+++ new/startup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#CREATE USERS.
+# username:passsword:Y
+# username2:password2:Y
+
+file="/home/createusers.txt"
+if [ -f $file ]
+  then
+    while IFS=: read -r username password is_sudo
+        do
+            echo "Username: $username, Password: $password , Sudo: $is_sudo"
+            useradd -ms /bin/bash $username
+            echo "$username:$password" | chpasswd
+            if [ "$is_sudo" = "Y" ]
+                then
+                    usermod -aG sudo $username
+            fi
+    done <"$file"
+fi
+
+#This has to be the last command!
+/usr/bin/supervisord -n
