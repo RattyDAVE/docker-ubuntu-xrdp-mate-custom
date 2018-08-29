72c1206252772dbfb773d657fbeaa7784e4cea79
Pedro Amador <pedroamador.rodriguez@gmail.com>
2018-08-29 17:07:15 +0000

    New: Configure local proxy in chromium-browser and firefox

---
+++ duing/firefox/syspref.js
@@ -0,0 +1,6 @@
+pref("network.proxy.http", "localhost");
+pref("network.proxy.http_port", 8118);
+pref("network.proxy.proxy_over_tls", false);
+pref("network.proxy.ssl", "localhost");
+pref("network.proxy.ssl_port", 8118);
+pref("network.proxy.type", 1);
