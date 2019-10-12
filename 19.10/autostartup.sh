5e7913109d7605a954066e95f47b6eebaeebeb69
Dave Pucknell <dave@pucknell.co.uk>
2019-10-12 11:43:38 +0100

    Fix the bugs.. Added more bugs

--- 19.10/autostartup.sh
+++ 19.10/autostartup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-uuidgen > /etc/machine-id
-
 #Set Timezone
 if [[ -z "${TZ}" ]]; then
    ln -fs /usr/share/zoneinfo/Europe/London /etc/localtime
@@ -35,65 +33,6 @@ if [ -f $file ]
             fi
     done <"$file"
 fi
-
-if [ ! -f "/etc/ssh/sshd_config" ];
-	then
-		cp /ssh_orig/sshd_config /etc/ssh
-fi
-
-if [ ! -f "/etc/ssh/ssh_config" ];
-	then
-		cp /ssh_orig/ssh_config /etc/ssh
-fi
-
-if [ ! -f "/etc/ssh/moduli" ];
-	then
-		cp /ssh_orig/moduli /etc/ssh
-fi
-
-# generate fresh rsa key if needed
-if [ ! -f "/etc/ssh/ssh_host_rsa_key" ];
-	then 
-		ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-fi
-
-# generate fresh dsa key if needed
-if [ ! -f "/etc/ssh/ssh_host_dsa_key" ];
-	then 
-		ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-fi
-
-#prepare run dir
-mkdir -p /var/run/sshd
-
-# generate xrdp key
-if [ ! -f "/etc/xrdp/rsakeys.ini" ];
-	then
-		xrdp-keygen xrdp auto
-fi
-
-# generate certificate for tls connection
-if [ ! -f "/etc/xrdp/cert.pem" ];
-	then
-		# delete eventual leftover private key
-		rm -f /etc/xrdp/key.pem || true
-		cd /etc/xrdp
-		# TODO make data in certificate configurable?
-		openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem -days 365 \
-		-subj "/C=US/ST=Some State/L=Some City/O=Some Org/OU=Some Unit/CN=Terminalserver"
-		crudini --set /etc/xrdp/xrdp.ini Globals security_layer tls
-		crudini --set /etc/xrdp/xrdp.ini Globals certificate /etc/xrdp/cert.pem
-		crudini --set /etc/xrdp/xrdp.ini Globals key_file /etc/xrdp/key.pem
-
-fi
-
-# generate machine-id
-uuidgen > /etc/machine-id
-
-# set keyboard for all sh users
-echo "export QT_XKB_CONFIG_ROOT=/usr/share/X11/locale" >> /etc/profile
-
-
 
 startfile="/root/startup.sh"
 if [ -f $startfile ]
