f5cdbc777acafc43b82002510f6e4826af918a0d
RattyDAVE <dave@pucknell.co.uk>
2020-04-06 07:58:37 +0100

    Update autostartup.sh

--- 20.04/autostartup.sh
+++ 20.04/autostartup.sh
@@ -48,5 +48,11 @@ fi
 
 echo "export QT_XKB_CONFIG_ROOT=/usr/share/X11/locale" >> /etc/profile
 
+# Create the PrivSep empty dir if necessary
+if [ ! -d /run/sshd ]; then
+   mkdir /run/sshd
+   chmod 0755 /run/sshd
+fi
+
 #This has to be the last command!
 /usr/bin/supervisord -n
