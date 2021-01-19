2b679c1f1171e07ed160a24a5112d4ca47edb7e9
subinps <64341611+subinps@users.noreply.github.com>
2021-01-19 17:14:49 +0530

    Create xrdp-start.sh

---
+++ xrdp-start.sh
@@ -0,0 +1,15 @@
+startfile="/root/startup.sh"
+if [ -f $startfile ]
+  then
+    sh $startfile
+fi
+
+echo "export QT_XKB_CONFIG_ROOT=/usr/share/X11/locale" >> /etc/profile
+
+# Create the PrivSep empty dir if necessary
+if [ ! -d /run/sshd ]; then
+   mkdir /run/sshd
+   chmod 0755 /run/sshd
+fi
+
+/usr/bin/supervisord -n
