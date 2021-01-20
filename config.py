93348a35f15108fa84aaf712e07fefa665801877
subinps <64341611+subinps@users.noreply.github.com>
2021-01-20 15:11:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,38 +1,27 @@
-# Just copy this file to config.py and change it to your info.
+import ast
+from os import environ
 from pyrogram import filters
 
-# Get these two from https://my.telegram.org
-API_ID = 1234567
-API_HASH = "ab1c23def45fg67890h123i45678j9kl"
 
-# Get this from @Botfather
-TOKEN = "1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI"
+API_ID = environ.get('API_ID') # Get these two from https://my.telegram.org
+API_HASH = environ.get('API_HASH') # Get these two from https://my.telegram.org
+TOKEN = environ.get('TOKEN')   ## Get this from @Botfather
+SUDO_USERS = environ.get('SUDO_USERS') # The IDs of the users which can stream, skip, pause and change volume
+GROUP = environ.get('GROUP') # The ID of the group where your bot streams
+MONGO_DB_URI = environ.get('MONGO_DB_URI') # Your mongodb uri
+USERS_MUST_JOIN = environ.get('USERS_MUST_JOIN', 'False') # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
+LOG = environ.get('LOG', 'True') # Send "Now playing" messages to the group you gave above
+LANG = environ.get('LANG', 'en') # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
+DUR_LIMIT = environ.get('DUR_LIMIT', 10) # Max video duration allowed for user downloads in minutes
 
-# Your mongodb uri
-MONGO_DB_URI = ""
 
-# The IDs of the users which can stream, skip, pause and change volume
-SUDO_USERS = [
-    383407735,
-    951435494,
-    1392620345
-]
 
-# The ID of the group where your bot streams
-GROUP = -1001402753006
 
-# Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
-USERS_MUST_JOIN = False
-
-# Send "now playing" messages to the group
-LOG = True
-
-# Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
-LANG = "en"
-
-# Max video duration allowed for user downloads in minutes
-DUR_LIMIT = 5
-
-# No need to touch the following.
+## No need to touch the following.
+SUDO_USERS = list(map(int, SUDO_USERS.split()))
+GROUP = int(GROUP)
+DUR_LIMIT = int(DUR_LIMIT)
+LOG = ast.literal_eval(LOG)
+USERS_MUST_JOIN = ast.literal_eval(USERS_MUST_JOIN)
 LOG_GROUP = GROUP if LOG else None
 SUDO_FILTER = filters.user(SUDO_USERS)
