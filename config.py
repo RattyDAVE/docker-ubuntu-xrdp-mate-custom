7c17e19daa6556dba624fd7703a028c58916d43d
subinps <64341611+subinps@users.noreply.github.com>
2021-01-21 01:35:07 +0530

    environment variables

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ SUDO_USERS = environ.get('SUDO_USERS') # The IDs of the users which can stream, 
 GROUP = environ.get('GROUP') # The ID of the group where your bot streams. eg:-1001402753006
 MONGO_DB_URI = environ.get('MONGO_DB_URI') # Your mongodb uri. eg:mongodb+srv://username:password@vcpb.opda3.mongodb.net/<dbname>?retryWrites=true&w=majority
 USERS_MUST_JOIN = environ.get('USERS_MUST_JOIN', 'False') # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
-LOG = environ.get('LOG', 'True') # Send "Now playing" messages to the group you gave above
+LOG = environ.get('LOG', 'False') # Send "Now playing" messages to the group you gave above
 LANG = environ.get('LANG', 'en') # Choose the preferred language for your bot. If English leave as it is, or change to the code of any supported language.
 DUR_LIMIT = environ.get('DUR_LIMIT', 10) # Max video duration allowed for user downloads in minutes
 
@@ -18,7 +18,10 @@ DUR_LIMIT = environ.get('DUR_LIMIT', 10) # Max video duration allowed for user d
 
 
 ## No need to touch the following.
+APP_ID = int(APP_ID)
 SUDO_USERS = list(map(int, SUDO_USERS.split()))
+if type(GROUP) is str:
+  GROUP = int(GROUP)
 GROUP = int(GROUP)
 DUR_LIMIT = int(DUR_LIMIT)
 LOG = ast.literal_eval(LOG)
