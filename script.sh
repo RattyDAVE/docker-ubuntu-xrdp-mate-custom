02b8aa6b9074a7d8ebff11e7d10b37aacfef1449
subinps <64341611+subinps@users.noreply.github.com>
2021-01-20 20:03:36 +0530

    Loading MySink And starting bot

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ user=$1vcpb
 exec su "$user" "$0" -- "$@"
 fi
 echo "This will be run from user $UID"
-mv /home/Telegram /home/vcpb/Telegram #Move Telegram to desktop
+mv /home/Telegram /home/vcpb/Telegram #Move Telegram to VCPB's Home
 pactl load-module module-null-sink sink_name=MySink #loading MySink
 pactl set-default-sink MySink #setting Mysink as default Mic
 cd /home/vcbot 
